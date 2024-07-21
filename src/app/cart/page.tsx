5936e4b88032b53a316ad0960d631ad94ba98315
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-21 10:56:30 -0300

    feat: create cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,10 @@
+import Page from "@/components/template/Page";
+
+export default function PageCart(){
+    return(
+        <Page>
+            <h1>Carrinho</h1>
+            <p>Conteúdo do carrinho</p>
+        </Page>
+    )
+}
