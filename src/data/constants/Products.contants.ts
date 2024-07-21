ab0b67e9e7554a8049d9f0a60a4992f9e37f202d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-21 20:24:30 -0300

    fix: export default

--- src/data/constants/Products.contants.ts
+++ src/data/constants/Products.contants.ts
@@ -86,3 +86,5 @@ const product: Product[] = [
       "https://images.unsplash.com/photo-1684360558160-a614e0c3c205?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjbGFkb3MlMjBtZWNhbmljb3N8ZW58MHx8MHx8fDA%3D",
   },
 ];
+
+export default product;
